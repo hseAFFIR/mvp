92cc9de5f09894f7141ce5fe3d2800be2aa0a977
defonisek <defonisek@mail.ru>
2025-01-10 15:20:43 +0300

    - added fileprocessor that sends data files to tokenizer

--- affir_mvp/__main__.py
+++ affir_mvp/__main__.py
@@ -1,7 +1,16 @@
 from affir_mvp.modules.tokenizer_factory import TokenizerFactory
 from affir_mvp.strategy import Strategy
+from affir_mvp.modules.file_reader import FileProcessor
+import os
 
 if __name__ == "__main__":
+    root_path = os.path.dirname(os.path.dirname(__file__))
+    folder_path = os.path.join(root_path, "data")
     factory = TokenizerFactory()
     tokenizer = factory.create_pipeline(Strategy.HIGH)
-    print(tokenizer.run("Проверка, текста"))
+    processor = FileProcessor(folder_path, tokenizer)
+    
+    # Вывод
+    for file_id, tokens in processor.process_files():
+        print(f"ID файла: {file_id}")
+        print(f"Токены: {tokens}")
