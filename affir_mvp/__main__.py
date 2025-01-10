7e121647dbd76aebee7c5362ea98ee7be8f811d1
Alexander Menkov <51756078+SunnyMenkov@users.noreply.github.com>
2025-01-10 16:19:20 +0300

    added load & save functions to indexer

--- affir_mvp/__main__.py
+++ affir_mvp/__main__.py
@@ -1,6 +1,7 @@
 import os
 
 import affir_mvp.indexer as indexer
+from affir_mvp.indexer import TOKEN_STORAGE, store_token, load_token_storage_from_file
 from affir_mvp.modules.file_processor import FileProcessor
 from affir_mvp.modules.tokenizer_factory import TokenizerFactory
 from affir_mvp.strategy import Strategy
@@ -13,3 +14,7 @@ if __name__ == "__main__":
     processor = FileProcessor(folder_path, tokenizer)
     processor.process_files()
     print(indexer.get_token_info("семья"))
+
+
+    #load_token_storage_from_file("token_storage.pkl")
+    #print(f"TOKEN_STORAGE: {TOKEN_STORAGE}")
--- affir_mvp/indexer.py
+++ affir_mvp/indexer.py
@@ -1,5 +1,8 @@
-TOKEN_STORAGE = dict()
+import pickle
 
+global TOKEN_STORAGE
+
+TOKEN_STORAGE = dict()
 
 def store_token(token: str, file_id: int, position: int):
     if token not in TOKEN_STORAGE:
@@ -9,6 +12,21 @@ def store_token(token: str, file_id: int, position: int):
     else:
         TOKEN_STORAGE[token][file_id] = {position}
 
-
 def get_token_info(token: str) -> dict | None:
     return TOKEN_STORAGE.get(token)
+
+def save_token_storage_to_file(filepath: str):
+    try:
+        with open(filepath, 'wb') as file:
+            pickle.dump(TOKEN_STORAGE, file)
+        print(f"Структура успешно сохранена в файл: {filepath}")
+    except Exception as e:
+        print(f"Ошибка при сохранении структуры в файл: {e}")
+
+def load_token_storage_from_file(filepath: str):
+    try:
+        with open(filepath, 'rb') as file:
+            TOKEN_STORAGE = pickle.load(file)
+        print(f"Структура успешно загружена из файла: {filepath}")
+    except Exception as e:
+        print(f"Ошибка при загрузке структуры из файла: {e}")
