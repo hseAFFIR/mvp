b65e9867d61fc252a6a963d20841386582ad62f3
GithubDenis5 <gromacihindenis@gmail.com>
2025-01-26 15:13:11 +0300

    test: search with htmler

--- tests/test_affir/test_htmler.py
+++ tests/test_affir/test_htmler.py
@@ -1,5 +1,9 @@
+from os.path import dirname, join
+
 import pytest
 
+from affir_mvp.file_processor import FileProcessor
+from affir_mvp.search import search
 from affir_mvp.tokenizer.filters import Htmler
 from affir_mvp.tokenizer.pipeline import TokenizerPipeline
 
@@ -17,13 +21,62 @@ def test_tokenizer_without_htmler():
     "word, expected_output",
     [
         ("<div>", None),  # строка с тегами и длиной меньше 10
-        (
-            '<img src="1/2/3/4/5/6/7/8/9/10">',
-            '<img src="1/2/3/4/5/6/7/8/9/10">',
-        ),  # строка с тегами и длиной больше 10
+        ("<", "<"),  # строка для тега длинной больше 10(обрезаеться в токинезаторе)
     ],
 )
 def test_tokenizer_with_htmler(word, expected_output):
     tokenizer = TokenizerPipeline(Htmler())  # с Htmler
     filtered_token = tokenizer.apply_filters(word)
     assert filtered_token == expected_output
+
+
+@pytest.mark.parametrize(
+    "word, expected_results",
+    [
+        (
+            "HTML",
+            {
+                "file1.txt:2": {55},
+                "file2.txt:1": {72, 122},
+            },
+        ),
+        (
+            "example",
+            {
+                "file2.txt:1": {109},
+            },
+        ),
+        (
+            "simple",
+            {
+                "file1.txt:2": {132},
+            },
+        ),
+        (
+            '<img src="path/folder/lib/png.zip">',
+            {
+                "file1.txt:2": {75},
+            },
+        ),
+        (
+            "<body>",
+            {},
+        ),
+    ],
+)
+def test_search_with_htmler(word, expected_results):
+    tokenizer = TokenizerPipeline(Htmler())
+
+    root_path = dirname(dirname(__file__))
+    folder_path = join(root_path, "tests_data/htmler")
+
+    processor = FileProcessor(folder_path)
+    processor.process_files(tokenizer)
+
+    # Запуск поиска
+    result = search(word, tokenizer)
+
+    # Проверка, что для каждого слова мы получаем правильные файлы и позиции
+    for file_key, expected_positions in expected_results.items():
+        assert file_key in result  # Проверяем, что файл присутствует в результатах
+        assert result[file_key] == expected_positions  # Проверяем, что позиции совпадают
